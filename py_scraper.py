03ff4d830d4c68c6590716980cf16ef705235f1e
JCode89 <JV.Colon@Yahoo.com>
2015-06-12 22:10:40 -0400

    Functions now have return.

--- py_scraper.py
+++ py_scraper.py
@@ -11,17 +11,20 @@ def get_site(url):
 	site = requests.get(url)
 	soup = BeautifulSoup(site.content)
 	return soup
+
 def scrape_links(soup, tag):
-	
 	with open(source, 'w') as f:
 		for item in soup.find_all(tag, id="archive-core"):
 			new_item = (str(item.find_all({'a':'href'})))
 			print new_item
 			for text in new_item:
 				f.write(text)
-				
+	return source			
+
 def scrape_text():
 	pass
+			
+			
 # Creates an empty dictionary				
 fin = {}
 # Rewrites the data from the file created by scrape and adds the data to the dictionary.
@@ -34,13 +37,14 @@ def rewrite(source):
 				value = lines[:lines.find('http')]
 				print (key, value)
 				fin[key] = [value]
+	return source
 
 # Creates a new file and saves the keys from the dictionary fin to it.				
 def new_file(s_data):
 	with open(s_data, 'w') as m:
 		for piece in fin:
 			m.write(('#[{}]\n').format(piece))
-
+	return s_data
 			
 url = raw_input('Enter the URL: ')			
 tag = 'section'	
