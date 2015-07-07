f5f10cede6deb8db42aa94a32b7bcb0a86efa7db
JCode89 <JV.Colon@Yahoo.com>
2015-07-06 21:35:11 -0400

    Tracks the length of the birth date.

--- WikiCapture.py
+++ WikiCapture.py
@@ -3,13 +3,14 @@ import requests
 import re
 
 class Scrape():
-    def __init__(self, site_name=input, rspns=input):
+    def __init__(self, site_name=input, rspns=input, display=print):
         self.site_name = site_name
         self.rspns = rspns
+        self.display = display
         self.birth_date = []
     def grab_birth_year(self):
-        name = self.site_name("Please enter name like this: First_Last:\n")
-        self.url = requests.get('https://en.wikipedia.org/wiki/'+ name)
+        self.name = self.site_name("Please enter name like this: First_Last:\n")
+        self.url = requests.get('https://en.wikipedia.org/wiki/'+ self.name)
         self.soup = BeautifulSoup(self.url.text)
         stuff = self.soup.find_all("span", class_="bday")
         num = re.compile('[0-9]{4}')
@@ -19,28 +20,31 @@ class Scrape():
                 
                 for date in birth:
                    self.birth_date.append(date)
+            self.date_of_birth = len("".join(self.birth_date)) # length of list should be 10
             birth = num.match(birth)
             birth_prime = birth.group()
-            print ("Born: %s" % birth_prime)
+            return self.display("Born: %s" % birth_prime), self.date_of_birth # prints out the year
         except UnboundLocalError:       
             try:
                 new_stuff = self.soup.find("tr")
                 n_birth = new_stuff.next_sibling.next_sibling.next_sibling.next_sibling
-                print (n_birth.next_element.next_element.next_element.next_element.next_element.next_element)
+                self.display(n_birth.next_element.next_element.next_element.next_element.next_element.next_element)
                 resp = self.rspns("Did it work?\n").lower()
+                
                 if resp == "yes":
                     exit(0)
                 elif resp == "no":
-                    print ("Here we are: %s" % n_birth.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element)				
-                    resp = self.rspns("How about that?\n").lower()                
+                    self.display("Here we are: %s" % n_birth.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element)				
+                    resp = self.rspns("How about that?\n").lower()
+                    				
                     if resp == "no":
                         n_birth = new_stuff.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling
-                        print ("Okay here it is: %s" % n_birth.next_element.next_element.next_element.next_element.next_element.next_element)
+                        return self.display("Okay here it is: %s" % n_birth.next_element.next_element.next_element.next_element.next_element.next_element)
                     else:
                         exit(0)
             except AttributeError:
-                print ("Woops! Date unfetchable!")
+                return self.display("Woops! Date unfetchable!")
 			
     
-prjct = Scrape()
-prjct.grab_birth_year()
+#prjct = Scrape()
+#prjct.grab_birth_year()
